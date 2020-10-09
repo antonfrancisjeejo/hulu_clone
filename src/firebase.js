c0526bd95e9afa766b27d1bdb4e3b1f77e1b60d4
unknown <anton13jeejo@gmail.com>
2020-10-09 20:03:34 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDb8TueAjddFijf3fZbg7p4uTR4z0NxNwY",
+  authDomain: "hulu-clone-6247d.firebaseapp.com",
+  databaseURL: "https://hulu-clone-6247d.firebaseio.com",
+  projectId: "hulu-clone-6247d",
+  storageBucket: "hulu-clone-6247d.appspot.com",
+  messagingSenderId: "102443340673",
+  appId: "1:102443340673:web:4e21db626acd2ef4244e80",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
